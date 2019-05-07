2dba56a31848f4cc4bd5a4291a17fab76e6574bb
Marek Tutaj <mtutaj@mcw.edu>
2019-05-07 15:05:32 -0500

    variant table name for human renamed to variant_clinvar

--- src/main/dist/_run.sh
+++ src/main/dist/_run.sh
@@ -13,8 +13,7 @@ if [ "$SERVER" == "REED" ]; then
     ELIST="mtutaj@mcw.edu"
 fi
 
-DB_OPTS="-Dspring.config=$APPDIR/../properties/default_db.xml"
-LOG4J_OPTS="-Dlog4j.configuration=file://$APPDIR/properties/log4j.properties"
-declare -x "GENE_LOCI_PIPELINE_OPTS=$DB_OPTS $LOG4J_OPTS"
-bin/$APPNAME "$@" > cron.log 2>&1
+java -Dspring.config=$APPDIR/../properties/default_db.xml \
+    -Dlog4j.configuration=file://$APPDIR/properties/log4j.properties \
+    -jar lib/$APPNAME.jar "$@" > cron.log 2>&1
 
